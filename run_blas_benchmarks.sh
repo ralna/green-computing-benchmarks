623fe39fadbe978a481c118a552fdfdccc1c7e27
Adam Greenbank <adam.greenbank@stfc.ac.uk>
2025-05-30 12:57:19 +0000

    Rework project structure Separate misc benchmarks that only run once and rename executables

--- run_blas_benchmarks.sh
+++ run_blas_benchmarks.sh
@@ -5,6 +5,6 @@ rm results_*.csv
 for B in ${BLAS_BACKENDS[@]};
 do
     export FLEXIBLAS=$B
-    ./build/green_computing_benchmark "${B}"
-    mpirun -np 4 ./build/green_computing_benchmark_mpi "${B}"
+    ./build/benchmark_blas "${B}"
+    mpirun -np 4 ./build/benchmark_mpi "${B}"
 done
