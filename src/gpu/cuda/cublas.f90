f0f7307fabb40bf549ab1ab7d057c9ce12681054
Adam Greenbank <adam.greenbank@stfc.ac.uk>
2026-05-21 14:18:17 +0000

    Add error checking to cublas

--- src/gpu/cuda/cublas.f90
+++ src/gpu/cuda/cublas.f90
@@ -35,6 +35,15 @@ module cublas_benchmarks
    end type CUBLASDGEMMBenchmark
 
 contains
+   subroutine cuda_err_check(stat, flag)
+      integer, intent(in) :: stat
+      integer, intent(out) :: flag
+
+      if ( stat /= 0 ) then
+         print *, "CUDA failed with error: ", stat
+         flag = 1
+      end if
+   end subroutine cuda_err_check
 
    !!!!!!!!!!!!!!!!
    ! CUBLAS DGEMM !
@@ -52,16 +61,19 @@ contains
       class(CUBLASDGEMMBenchmark), intent(inout) :: self
       character(len=16), intent(in) :: blas_name
 
-      ! status codes
-      integer(c_int) :: istat
-
       integer :: i, j, k, iunit
       real(real64) :: avg_gflops
       logical :: file_exists
+      integer :: flag = 0
 
       ! init CUBLAS handle
       self%handle = c_null_ptr
-      istat = cublasCreate(self%handle)
+      call cuda_err_check(cublasCreate(self%handle), flag)
+
+      if ( flag == 1 ) then
+         print *, "Failed to create CUBLAS handle"
+         return
+      end if
 
       call self%open_results_file(iunit, file_exists)
 
@@ -92,34 +104,49 @@ contains
                call random_number(self%C)
 
                ! Allocate device memory
-               istat = cudaMalloc(self%A_d,&
-                  int(self%m*self%k, c_size_t)*c_sizeof(self%A(1,1)))
-               istat = cudaMalloc(self%B_d,&
-                  int(self%k*self%n, c_size_t)*c_sizeof(self%B(1,1)))
-               istat = cudaMalloc(self%C_d,&
-                  int(self%m*self%n, c_size_t)*c_sizeof(self%C(1,1)))
+               call cuda_err_check(cudaMalloc(self%A_d,&
+                  int(self%m*self%k, c_size_t)*c_sizeof(self%A(1,1))),&
+                  flag)
+               call cuda_err_check(cudaMalloc(self%B_d,&
+                  int(self%k*self%n, c_size_t)*c_sizeof(self%B(1,1))),&
+                  flag)
+               call cuda_err_check(cudaMalloc(self%C_d,&
+                  int(self%m*self%n, c_size_t)*c_sizeof(self%C(1,1))),&
+                  flag)
 
                ! Copy to device
-               istat = cudaMemcpy(self%A_d,&
-                  c_loc(self%A(1,1)), int(self%m*self%k, c_size_t)*c_sizeof(self%A(1,1)), cudaMemcpyHostToDevice)
+               call cuda_err_check(cudaMemcpy(self%A_d,&
+                  c_loc(self%A(1,1)), int(self%m*self%k, c_size_t)*c_sizeof(self%A(1,1)), cudaMemcpyHostToDevice),&
+                  flag)
 
-               istat = cudaMemcpy(self%B_d,&
-                  c_loc(self%B(1,1)), int(self%k*self%n, c_size_t)*c_sizeof(self%B(1,1)), cudaMemcpyHostToDevice)
+               call cuda_err_check(cudaMemcpy(self%B_d,&
+                  c_loc(self%B(1,1)), int(self%k*self%n, c_size_t)*c_sizeof(self%B(1,1)), cudaMemcpyHostToDevice),&
+                  flag)
 
-               istat = cudaMemcpy(self%C_d,&
-                  c_loc(self%C(1,1)), int(self%m*self%n, c_size_t)*c_sizeof(self%C(1,1)), cudaMemcpyHostToDevice)
+               call cuda_err_check(cudaMemcpy(self%C_d,&
+                  c_loc(self%C(1,1)), int(self%m*self%n, c_size_t)*c_sizeof(self%C(1,1)), cudaMemcpyHostToDevice),&
+                  flag)
 
-               print *, istat
+               if ( flag == 1 ) then
+                  print *, "Failed to run CUBLAS DGEMM"
+                  avg_gflops = -1
+               else
+                  avg_gflops = self%time_benchmark(100)
+               end if
+
+               call cuda_err_check(cudaFree(self%A_d), flag)
+               call cuda_err_check(cudaFree(self%B_d), flag)
+               call cuda_err_check(cudaFree(self%C_d), flag)
 
                write(iunit, '(F13.7,A)', advance='no') avg_gflops, ','
-
-               istat = cudaFree(self%A_d)
-               istat = cudaFree(self%B_d)
-               istat = cudaFree(self%C_d)
 
                deallocate(self%A)
                deallocate(self%B)
                deallocate(self%C)
+
+               if (flag == 1) then
+                  return
+               end if
             end do
          end do
       end do
